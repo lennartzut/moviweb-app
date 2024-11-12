2bd32bb2f65145474679218a41d7333092559237
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-11-12 22:15:46 +0100

    Adds movie search results when adding movie, displaying multiple options from the OMDB API which can then be added.

--- api/api.py
+++ api/api.py
@@ -6,13 +6,14 @@ load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 
-
-def make_api_request(movie_title):
+def make_api_request(query, by_id=False):
     """
-    Request movie data from OMDb API.
+    Request movie data from OMDb API based on title or IMDb ID.
 
     Args:
-        movie_title (str): The title of the movie.
+        query (str): The title keyword or IMDb ID to search for.
+        by_id (bool): If True, searches using IMDb ID.
+                      If False, searches by title.
 
     Returns:
         dict: JSON response with movie data or None if there's an error.
@@ -21,11 +22,25 @@ def make_api_request(movie_title):
         print("Error: API_KEY is not set. Please check your .env file.")
         return None
 
-    api_url = f"http://www.omdbapi.com/?apikey={API_KEY}&t={movie_title}"
+    # Adjust the URL based on whether we are searching by title or IMDb ID
+    if by_id:
+        api_url = f"http://www.omdbapi.com/?apikey={API_KEY}&i={query}"
+    else:
+        api_url = f"http://www.omdbapi.com/?apikey={API_KEY}&s={query}"
+
     try:
         response = requests.get(api_url, timeout=5)
         if response.status_code == requests.codes.ok:
-            return response.json()
+            data = response.json()
+            if data.get("Response") == "True":
+                # If by_id is True, return the full movie data directly
+                if by_id:
+                    return data
+                # Otherwise, return the list of search results
+                return data.get("Search", [])
+            else:
+                print("No results found:", data.get("Error"))
+                return None
         else:
             print("Error:", response.status_code, response.json())
             return None
